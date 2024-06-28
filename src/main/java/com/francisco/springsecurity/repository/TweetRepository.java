1384942650e8cba4f7451a7210e99f2470c9577c
Francisco Sousa <franciscoeds1987@gmail.com>
2024-06-28 11:36:08 -0300

    Repositórios das entities criados

---
+++ src/main/java/com/francisco/springsecurity/repository/TweetRepository.java
@@ -0,0 +1,9 @@
+package com.francisco.springsecurity.repository;
+
+import com.francisco.springsecurity.entities.Tweet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TweetRepository extends JpaRepository<Tweet, Long> {
+}
