effcbfb14e9b90b23329938ba1b637b50969a299
Francisco Sousa <franciscoeds1987@gmail.com>
2024-06-28 11:06:51 -0300

    Entidades criadas (User, Role e Tweet)

---
+++ src/main/java/com/francisco/springsecurity/entities/Tweet.java
@@ -0,0 +1,56 @@
+package com.francisco.springsecurity.entities;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "tb_tweets")
+public class Tweet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "tweet_id")
+    private Long tweetId;
+
+    private User user;
+
+    private String content;
+
+    @CreationTimestamp
+    private Instant creationTimeStamp;
+
+    public Long getTweetId() {
+        return tweetId;
+    }
+
+    public void setTweetId(Long tweetId) {
+        this.tweetId = tweetId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Instant getCreationTimeStamp() {
+        return creationTimeStamp;
+    }
+
+    public void setCreationTimeStamp(Instant creationTimeStamp) {
+        this.creationTimeStamp = creationTimeStamp;
+    }
+
+}
